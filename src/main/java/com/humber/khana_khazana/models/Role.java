c02fddc9254c314689d43f2bf49844cdcbdc727d
janki patel <jankipatel1065@gmail.com>
2023-12-29 14:06:25 -0500

    add user and role model

--- src/main/java/com/humber/khana_khazana/models/Role.java
+++ src/main/java/com/humber/khana_khazana/models/Role.java
@@ -1,2 +1,21 @@
-package com.humber.khana_khazana.models;public class Role {
+package com.humber.khana_khazana.models;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotEmpty;
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+@Entity
+@Table(name = "roles")
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+    @Column(nullable = false,unique = true)
+    @NotEmpty
+    private String name;
+    @ManyToMany(mappedBy = "roles")
+    private List<User> users;
 }
